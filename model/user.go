ca2f4ebcc8936db91391b4dc4340180f2c27ed7c
s747296681 <s747296681@vip.qq.com>
2022-06-14 16:24:15 +0800

    adjust code

--- model/user.go
+++ model/user.go
@@ -12,7 +12,7 @@ type User struct {
 	PassWord      string `json:"pass_word,omitempty" gorm:"type:varchar(255);"`
 	FollowCount   int64  `json:"follow_count,omitempty"`
 	FollowerCount int64  `json:"follower_count,omitempty"`
-	IsFollow      bool   `json:"is_follow,omitempty"`
+	IsFollow      bool   `json:"is_follow,omitempty" gorm:"-"`
 	CommentList   []Comment
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
